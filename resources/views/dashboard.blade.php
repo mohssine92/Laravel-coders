56357401a9dd0c2173b795c5f41b76ef98c2e323
mohssine92 <mohssinelmariouh@gmail.com>
2021-04-13 18:43:23 +0200

    hemos visto componetesBlade que generamos son 2 tipos componentes de class y componentes anonimos . como tenemos laravel instalado con paquete de jestream , hemos publicado los componentes de jestream asi la tenemos disponibles par  instanciarlos siempre cuando necesitamos alguno de ellos . tambien hay componentes de livewire

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,14 @@
-<x-app-layout>   {{--  vista dashboard de jestream su estructura esta creada por componentes de blade  --}}
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<x-app-layout>                    {{--  vista dashboard de jestream su estructura esta creada por componentes de blade  --}}
 
+
+     {{-- slot --}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <x-jet-welcome />  {{-- inst comp de blde jestream x-jet --}}
             </div>
         </div>
     </div>
-</x-app-layout>
+
+
+</x-app-layout> {{-- esta es la forma de llamar componentes de blade , sea componente de classe o componente anonimo  --}}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>  {{-- apartir de laravel 8 , este archivo se instacia como componente de blade , ya no se extiende .asi esta es vista de componente de classe--}}
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">  {{-- retorna valor config/app.php {'locale' => 'en',} --}}
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>  {{-- retorna valores .env --}}
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">  {{-- mix() = asset() se posicionan en public/+concatenacion  --}}
 
-        @livewireStyles
+        @livewireStyles  {{-- aqua esta incorporando los estilos de livewire --}}
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script> {{-- defer => pasarle valor defer es justo si hubiera poner script al final del body  --}}
     </head>
 
     <body class="font-sans antialiased">
+
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
 
-            @livewire('navigation-menu')  {{-- component de livewire carga esta vista apartir de views/ --}}
+            @livewire('navigation-menu')   {{--esta es la forma de instanciar o llamar componente de livewire--}}
 
 
 
@@ -32,11 +34,13 @@
             <main>
                 {{ $slot }}
             </main>
+
         </div>
 
         @stack('modals')
 
         @livewireScripts
+
     </body>
 
 </html>
