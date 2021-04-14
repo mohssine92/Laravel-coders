c3f7a11496e0ca4bfb5b5a81b287f06a53e498fa
mohssine92 <mohssinelmariouh@gmail.com>
2021-04-14 10:52:33 +0200

    11 - multi uso de componente de blade jestream , uso de directiva de blade de autenticacion , en cada archivo trabajamos codigo de dos pantallas separado

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,21 +2,20 @@
 @php
     $nav_links = [
          [
-           'name' => 'Dashboard',
-           'route' => route('dashboard'),  /* este metodo busqua en archivo de routas , ruta con este nombre y nos devuelva su url  */
-           'active' => request()->routeIs('dashboard')  /* este metodo verifique se actualmente nos encontramos en la ruta dashboard   return  true | false */
+           'name' => 'Home',
+           'route' => route('home'),  /* este metodo busqua en archivo de routas , ruta con este nombre y nos devuelva su url  */
+           'active' => request()->routeIs('home')  /* este metodo verifique se actualmente nos encontramos en la ruta dashboard   return  true | false */
          ],
-         [
-           'name' => 'Prueba',
-           'route' => '#',
-           'active' => false
-         ]
+
     ]
 @endphp
 
+
+
+
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow ">
 
-    <!-- Primary Navigation Menu , pantalla ordenador normal-->
+    <!--TODO : Primary Navigation Menu , pantalla ordenador normal-->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
@@ -35,8 +34,8 @@
                       @foreach ( $nav_links as $nav_link  )
 
                            {{-- component Jetstream  --}}
-                           <x-jet-nav-link href="{{ route('dashboard') }}" :active="$nav_link['active']">
-                               {{ $nav_link['name'] }}
+                           <x-jet-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
+                            {{ $nav_link['name'] }}
                            </x-jet-nav-link>
 
                       @endforeach
@@ -49,10 +48,12 @@
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+
                 <!-- Teams Dropdown -->
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                @if(Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ml-3 relative">
                         <x-jet-dropdown align="right" width="60">
+
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
@@ -64,6 +65,7 @@
                                     </button>
                                 </span>
                             </x-slot>
+
 
                             <x-slot name="content">
                                 <div class="w-60">
@@ -95,61 +97,79 @@
                                     @endforeach
                                 </div>
                             </x-slot>
+
                         </x-jet-dropdown>
                     </div>
                 @endif
 
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
+
+                @auth  {{-- directiva de blade muestra contenido solo cuando user esta autentificado --}}
+
                     <x-jet-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
-                            @else
-                                <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
-
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
+                            <x-slot name="trigger">
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                     </button>
-                                </span>
-                            @endif
-                        </x-slot>
+                                @else
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                            {{ Auth::user()->name }}
 
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                @endif
+                            </x-slot>
 
-                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-jet-dropdown-link>
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                </div>
 
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Profile') }}
                                 </x-jet-dropdown-link>
-                            @endif
 
-                            <div class="border-t border-gray-100"></div>
+                                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                    <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                        {{ __('API Tokens') }}
+                                    </x-jet-dropdown-link>
+                                @endif
 
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
+                                <div class="border-t border-gray-100"></div>
 
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
-                                         onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-jet-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-jet-dropdown>
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-jet-dropdown-link href="{{ route('logout') }}"
+                                             onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-jet-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-jet-dropdown>
+
+                    @else
+
+                    <x-jet-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+                         Login
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('register') }}" :active="request()->routeIs('register') ">
+                         Register
+                    </x-jet-nav-link>
+
+
+
+                    @endauth
+
                 </div>
             </div>
 
@@ -165,7 +185,11 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu Movil   -->
+
+
+
+
+    <!--TODO : Responsive Navigation Menu Movil   -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
 
@@ -182,7 +206,10 @@
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+
+      @auth
+           <div class="pt-4 pb-1 border-t border-gray-200">
+
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="flex-shrink-0 mr-3">
@@ -250,6 +277,24 @@
                     @endforeach
                 @endif
             </div>
-        </div>
+           </div>
+
+       @else
+
+       <div class="py-1 border-t border-gray-200">
+
+          <x-jet-responsive-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+              Login
+          </x-jet-responsive-nav-link>
+          <x-jet-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+             Register
+          </x-jet-responsive-nav-link>
+
+       </div>
+
+       </div>
+
+      @endauth
+
     </div>
 </nav>
